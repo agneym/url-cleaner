3d416e803529eba18a46a0adf3d1fb51aabea524
Agney Menon <agney@circle.co>
2022-06-01 01:25:35 +0530

    add input element HTML

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,28 @@
   <title>URL Cleaner</title>
 </svelte:head>
 
-<h1>URL Cleaner</h1>
-<style>
+<main class="container">
+  <h1 class="title">URL Cleaner</h1>
+  <input class="input" type="url" placeholder="Enter URL" />
+</main>
 
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    gap: 2rem;
+  }
+
+  .input {
+    all: unset;
+    background-color: var(--color-background);
+    border: 1px solid aliceblue;
+    padding: 4px 8px;
+    min-width: max(100%, 20rem);
+    border-radius: 0.35rem;
+  }
 </style>
 
 <script>
