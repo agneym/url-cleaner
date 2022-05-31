7fa78a1d3c12fa80279a477f53cbc461647fc99a
Agney Menon <agney@circle.co>
2022-06-01 00:54:53 +0530

    remove static favicon and add emoji

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,6 @@
+<svelte:head>
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧹</text></svg>">
+</svelte:head>
 <div class="container">
   <slot></slot>
 </div>
