74400e2ddba6a5494ea5582552ac5fa8a3e02042
Agney Menon <agney@circle.co>
2022-06-01 00:52:46 +0530

    Initial setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<div class="container">
+  <slot></slot>
+</div>
+
+<style>
+  .container {
+    display: grid;
+    height: 100%;
+    place-content: center;
+  }
+</style>
+
+<script global>
+  import "../global.css";
+</script>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>URL Cleaner</title>
+</svelte:head>
+
+<h1>Paste URL here</h1>
+<style>
+
+</style>
+
+<script>
+</script>
