8fd55f7b0e129e431273d901d5c1f4bd5972b87e
Agney Menon <agney@circle.co>
2022-06-17 21:52:30 +0530

    show URL and copy to clipboard

--- src/components/ShowUrl.svelte
+++ src/components/ShowUrl.svelte
@@ -1,6 +1,42 @@
-<h2 class="">Your cleaned URL:</h2>
-<p>{url}</p>
+<section class="container">
+  <h2 class="heading">Your cleaned URL</h2>
+  <p>{url}</p>
+  <button class="button" type="button" on:click={copyToClipboard}
+    >Copy to Clipboard</button
+  >
+</section>
 
 <script>
+  import {notifications} from '../lib/notifications.js';
+
   export let url;
+
+  async function copyToClipboard() {
+    if (!navigator.clipboard) {
+      notifications.danger("We could not access the clipboard. Sorry.");
+      return;
+    }
+    try {
+      await navigator.clipboard.writeText(url);
+      notifications.success("Copied to the clipboard");
+    } catch (err) {
+      notifications.danger('Failed to copy!', err)
+    }
+  }
 </script>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px 0;
+  }
+  .heading {
+    color: #5f6368;
+  }
+  .button {
+    all: unset;
+    cursor: pointer;
+  }
+</style>
